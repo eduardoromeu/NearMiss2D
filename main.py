e4a5b7e27cedbfcddc7292378f7367753999cb48
Dudu <eduardoromeueberts@gmail.com>
2025-08-21 21:41:36 -0300

    Pygame Install

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-import os
+import pygame
 
-print("Hello system!")
+print("Initializing game...")
 
-file_name = "./src/assets/a.txt"
+pygame.init()
 
-with open(file_name, 'r') as f:
-    content = f.read()
-  
-print(content)
+screen = pygame.display.set_mode((1024, 768), vsync=True)
+display_info =  pygame.display.Info()
 
+clock = pygame.time.Clock()
 
-input("Enter any key to continue...")
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("orange")
+
+    # RENDER YOUR GAME HERE
+    pygame.display.set_caption(f'NearMiss2d {pygame.display.get_driver()} @ {round(clock.get_fps())} fps ({display_info.current_w}, {display_info.current_h})')
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
